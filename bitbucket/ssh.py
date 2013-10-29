735386ef4be72c83c735e82395139ef6cc4be0da
Ilja Orlovs <ilja.orlovs@tells.so>
2013-10-29 22:16:13 +0000

    No longer expecting for the ssh key DELETE request to return JSON response.

--- bitbucket/ssh.py
+++ bitbucket/ssh.py
@@ -16,25 +16,25 @@ class SSH(base.Endpoint):
     def all(self):
         """ Get all ssh keys associated with your account.
         """
-        url = self.bitbucket.url('GET_SSH_KEYS')
+        url = self.urls('GET_SSH_KEYS')
         return self.bitbucket.dispatch(url)
 
     def get(self, key_id=None):
         """ Get one of the ssh keys associated with your account.
         """
-        url = self.bitbucket.url('GET_SSH_KEY', key_id=key_id)
+        url = self.urls('GET_SSH_KEY', key_id=key_id)
         return self.bitbucket.dispatch(url)
 
-    def create(self, key=None, label=None):
+    def create(self, key=None, label="unnamed key"):
         """ Associate an ssh key with your account and return it.
         """
-        key = '%s' % key
-        url = self.bitbucket.url('SET_SSH_KEY')
+        key = str(key)
+        url = self.urls('SET_SSH_KEY')
         return self.bitbucket.dispatch.post(url, data={"key": key, "label": label})
 
     def delete(self, key_id=None):
         """ Delete one of the ssh keys associated with your account.
             Please use with caution as there is NO confimation and NO undo.
         """
-        url = self.bitbucket.url('DELETE_SSH_KEY', key_id=key_id)
-        return self.bitbucket.dispatch.delete(url)
+        url = self.urls('DELETE_SSH_KEY', key_id=key_id)
+        return self.bitbucket.dispatch.delete(url, parse=False)
